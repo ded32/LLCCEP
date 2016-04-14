3f36e3a7ff3fa8c82dfec06fc6072664a55137fa
Andrew Bezzubtsev <andrew.bezzubtsev@icloud.com>
2016-04-14 17:16:04 +0300

    Bugfix

--- JIT/test.cpp
+++ JIT/test.cpp
@@ -4,8 +4,8 @@
 #include <cstring>
 
 #if defined(__linux__) || defined(__APPLE__)
-#include <sys/mman.h>
-#include <unistd.h>
+#	include <sys/mman.h>
+#	include <unistd.h>
 #endif
 
 #include "opcodes.cpp"
@@ -14,13 +14,13 @@
 ({                                         \
 	std::cout << #cmd << "... ";       \
                                            \
-	auto _ret = ({ cmd; });            \
+	auto _ret = ({cmd;});              \
                                            \
 	std::cout << "[" << _ret << "]\n"; \
 	_ret;                              \
 })
 
-__attribute__((__always_inline__)) inline int call(const uint8_t* memory)
+__attribute__((__always_inline__)) inline int call(const uint8_t *memory)
 {
 	asm __volatile__(
 		"call *%0"
@@ -48,7 +48,7 @@ int main()
 	$(sz);
 
 #if defined(__linux__) || (defined(__APPLE__) && defined(__MACH__))
-	$(memory = (uint8_t*)mmap(0, sz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANON, -1, 0));
+	$(memory = (uint8_t *)mmap(0, sz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANON, -1, 0));
 	$(std::copy(vec.begin(), vec.end(), memory));
 
 	$(call(memory));
@@ -60,5 +60,4 @@ int main()
 
 	return 0;
 }
-    
 
