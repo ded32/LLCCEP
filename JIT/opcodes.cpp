047bda6a4b0143c36801c2e74a744da10e4b8382
Andrew Bezzubtsev <andrew.bezzubtsev@icloud.com>
2016-04-14 22:42:15 +0300

    FPU support

--- JIT/opcodes.cpp
+++ JIT/opcodes.cpp
@@ -6,13 +6,91 @@
 #define APPEND_BYTE(vec, byte) vec.push_back(byte);
 
 namespace LLCCEP_JIT {
-	void append_imm32(bytevec& vec)
+	void append_imm32(bytevec &vec)
 	{
 		APPEND_BYTE(vec, 0x68)
 	}
 
-	void append_ret(bytevec& vec)
+	void append_ret(bytevec &vec)
 	{
 		APPEND_BYTE(vec, 0xC3)
+	}
+
+	void append_finit(bytevec& vec)
+	{
+		APPEND_BYTE(vec, 0x9B)
+		APPEND_BYTE(vec, 0xDB)
+		APPEND_BYTE(vec, 0xE3)
+	}
+
+	void append_fclex(bytevec &vec)
+	{
+		APPEND_BYTE(vec, 0x9B)
+		APPEND_BYTE(vec, 0xDB)
+		APPEND_BYTE(vec, 0xE2)
+	}
+
+
+#define FPU_STD_APPEND(vec) APPEND_BYTE(vec, 0x24)
+	void append_fadd(bytevec &vec)
+	{
+		APPEND_BYTE(vec, 0xDC)
+		APPEND_BYTE(vec, 4)
+		FPU_STD_APPEND(vec)
+	}
+
+	void append_fsub(bytevec &vec)
+	{
+		APPEND_BYTE(vec, 0xDC)
+		APPEND_BYTE(vec, 36)
+		FPU_STD_APPEND(vec)
+	}
+
+	void append_fmul(bytevec &vec)
+	{
+		APPEND_BYTE(vec, 0xDC)
+		APPEND_BYTE(vec, 12)
+		FPU_STD_APPEND(vec)
+	}
+
+	void append_fseg(bytevec &vec)
+	{
+		APPEND_BYTE(vec, 0xDC)
+		APPEND_BYTE(vec, 52)
+		FPU_STD_APPEND(vec)
+	}
+
+	void append_fld(bytevec &vec)
+	{
+		APPEND_BYTE(vec, 0xDD)
+		APPEND_BYTE(vec, 4)
+		FPU_STD_APPEND(vec)
+	}
+
+	void append_fstp(bytevec &vec)
+	{
+		APPEND_BYTE(vec, 0xDD)
+		APPEND_BYTE(vec, 28)
+		FPU_STD_APEND(vec)
+	}
+
+	void append_push(bytevec &vec, double *val)
+	{
+		uint8_t *bytes = static_cast<uint8_t *>(val);
+
+		append_imm32(vec);
+		for (unsigned i = 0; i < 4; i++)
+			APPEND_BYTE(bytes[i])
+
+		append_imm32(vec);
+		for (unsigned i = 4; i < 8; i++)
+			APPEND_BYTE(bytes[i])
+	}
+
+	void append_pop(bytevec &vec)
+	{
+		APPEND_BYTE(vec, 0x83)
+		APPEND_BYTE(vec, 0xC4)
+		APPEND_BYTE(vec, 0x8)
 	}
 }
--- JIT/opcodes.hpp
+++ JIT/opcodes.hpp
@@ -8,8 +8,20 @@ namespace LLCCEP_JIT {
 	typedef uint8_t BYTE;
 	typedef std::vector<BYTE> bytevec;
 
-	void append_imm32(bytevec& vec);
-	void append_ret(bytevec& vec);
+	void append_imm32(bytevec &vec);
+	void append_ret(bytevec &vec);
+
+	void append_finit(bytevec &vec);
+	void append_fclex(bytevec &vec);
+	void append_fadd(bytevec &vec);
+	void append_fsub(bytevec &vec);
+	void append_fmul(bytevec &vec);
+	void append_fseg(bytevec &vec);	
+	void append_fld(bytevec &vec);
+	void append_fstp(bytevec &vec);
+
+	void append_push(bytevec &vec, double *val);
+	void append_pop(bytevec &vec);	
 }
 
 #endif // OPCODES_HPP
