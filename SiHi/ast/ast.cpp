561f79ec7b2e48573af89613901e1f2996425686
Andrew Bezzubtsev <andrew.bezzubtsev@icloud.com>
2016-05-11 22:31:29 +0300

    AST class base

---
+++ SiHi/ast/ast.cpp
@@ -0,0 +1,35 @@
+#include <initializer_list>
+#include <vector>
+#include <string>
+
+#include <STLExtras.hpp>
+
+#include "ast.hpp"
+
+namespace LLCCEP_SiHi {
+	ast::ast():
+		__children__(),
+		__ancestor__(0),
+		__lex_data__()
+	{ }
+
+	ast::ast(const ast &src):
+		__children__(src.__children__),
+		__ancestor__(0),
+		__lex_data__(src.__lex_data__)
+	{ }
+	
+	ast::ast(std::initializer_list<ast *> children, int lex_t, ::std::string lex_val):
+		__children__(children),
+		__ancestor__(0),
+		__lex_data__({lex_t, lex_val})
+	{ }
+
+	ast::~ast()
+	{
+		if (__ancestor__)
+			for (size_t i = 0; i < __ancestor__->__children__.size(); i++)
+				if (__ancestor__->__children__[i] == this)
+					__ancestor__->__children__.erase(__ancestor__->__children__.begin() + i);
+	}
+}
---
+++ SiHi/ast/ast.hpp
@@ -0,0 +1,31 @@
+#ifndef AST_HPP
+#define AST_HPP
+
+#include <initializer_list>
+#include <vector>
+#include <string>
+
+namespace LLCCEP_SiHi {
+	class ast {
+		::std::vector<ast *> __children__;
+		ast *__anscestor__;
+
+		struct {
+			int __type__;
+			::std::string __val__;
+		} __lex_data__;
+	public:
+		ast();
+		ast(const ast &src);
+		ast(std::initializer_list<ast *> children, int lex_t, ::std::string lex_val);
+		~ast();
+
+		inline bool OK() const;
+
+		void insert_child(const ast *src);
+		::std::vector<ast *> get_children() const;
+		ast *get_ancestor() const; 
+	};
+}
+
+#endif // AST_HPP
