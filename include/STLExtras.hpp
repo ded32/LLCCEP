68e43af3803546c4d5a4ebd2dbb2e2eecdad4afb
Andrew Bezzubtsev <andrew.bezzubtsev@icloud.com>
2016-06-14 18:03:32 +0300

    STLExtras: get file length

--- include/STLExtras.hpp
+++ include/STLExtras.hpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <cstddef>
 #include <cstdint>
+#include <ifstream>
 
 template<typename TYPE>
 std::vector<TYPE> subvector(std::vector<TYPE> &src, size_t id0, size_t id1)
@@ -51,6 +52,24 @@ template<typename TYPE>
 uint8_t *to_bytes(TYPE &data)
 {
 	return static_cast<uint8_t *>(&data);
+}
+
+template<typename char_t>
+::std::basic_ifstream<char_t>::pos_type get_length(::std::basic_ifstream<char_t> &fd)
+{
+	if (fd.fail()) {
+		throw RUNTIME_EXCEPTION(CONSTRUCT_MSG(
+			"Error!\n"
+			"File reference with fail!\n"));
+	}
+
+	auto pos = fd.tellg();
+	fd.seekg(0, fd.end);
+	auto res = fd.tellg();
+
+	fd.seekg(0, pos);
+
+	return res;
 }
 
 #endif // STLEXTRAS_HPP
