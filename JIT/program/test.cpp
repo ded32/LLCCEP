081e53987405e6a73112f9a12239a1ba62f9c8a2
Andrew Bezzubtsev <andrew.bezzubtsev@icloud.com>
2016-06-26 11:59:14 +0300

    a new attempt of debugging

--- JIT/program/test.cpp
+++ JIT/program/test.cpp
@@ -4,19 +4,6 @@
 #include "../startup/startup.hpp"
 #include "../emitter/emitter.hpp"
 
-// finit
-// fclex
-// mov rbx, 0xFFFFFFFF
-// mov rax, rbx
-// push 0xFF
-// push rax
-// pop rax
-// pop rbx
-// mov rax, some_func
-// call rax
-// nop
-// ret
-
 void some_func()
 {
 
@@ -26,15 +13,13 @@ int main()
 {
 	LLCCEP_JIT::program prog;
 
-//	std::cout << std::hex << *(uint64_t *)&some_func << "\n";
+	std::cout << std::hex << *(uint64_t *)&some_func << "\n";
 
 	prog.emit_mov_reg_imm(LLCCEP_JIT::RAX, *(uint64_t *)&some_func);
 	prog.emit_call_reg(LLCCEP_JIT::RAX);
 
 	prog.emit_nop();
 	prog.emit_ret();
-
-	prog.dump(true);
 
 	auto data = LLCCEP_JIT::make_program(prog);
 	call(data);
