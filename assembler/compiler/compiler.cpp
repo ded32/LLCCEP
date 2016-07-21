0e607d0fbddb58ac88f1afe1e1b7ff13366e8607
Andrew Bezzubtsev <andrew.bezzubtsev@icloud.com>
2016-07-21 21:38:54 +0300

    new version of assembler

--- assembler/compiler/compiler.cpp
+++ assembler/compiler/compiler.cpp
@@ -7,11 +7,15 @@
 #include <fstream>
 #include <iostream>
 #include <cstring>
-#include <cerrno>
+#include <utility>
+
+#include <errno.h>
+#include <stddef.h>
 
 #include <STDExtras.hpp>
 
 #include "../lexer/lexer.hpp"
+#include "../linker/linker.hpp"
 #include "../codegen/codegen.hpp"
 
 namespace LLCCEP_ASM {
@@ -24,32 +28,31 @@ namespace LLCCEP_ASM {
 		}
 
 		::std::vector<lexem> program;
+		::std::vector<::std::pair<lexem, size_t> > labels_table;
 
-		for (uint64_t i = 1; !in.eof(); i++) {
-			::std::string code = "";
-			::std::getline(in, code);
-	
-			try {
-				to_lexems(code, program, 
-				          in_path, i);
-			} catch (::LLCCEP::runtime_exception &exc) {
-				throw (exc);
-			} DEFAULT_HANDLING
-	
-			if (!program.size())
-				continue;
+		try {
+			for (size_t i = 1; !in.eof(); i++) {
+				::std::string code = "";
+				::std::getline(in, code);
+				size_t old = labels_table.size();
 
-			try {
-				LLCCEP_ASM::op *prep = prepare_op(program);
+				to_lexems(code, program, in_path, i);
+				make_labels_associative_table(labels_table, program, i);
+
+				if (!program.size() || labels_table.size() != old)
+					continue;
+
+				substitute_labels_with_addresses(labels_table, program);
+
+				LLCCEP_ASM::op prep = prepare_op(program);
 				dump_bitset(out, prep);
-				delete prep;
-			} catch (::LLCCEP::runtime_exception &exc) {
-				throw (exc);
-			} DEFAULT_HANDLING
-
-			code.clear();
-			program.clear();
-		}
+			
+				code.clear();
+				program.clear();
+			}
+		} catch (::LLCCEP::runtime_exception &exc) {
+			throw (exc);
+		} DEFAULT_HANDLING
 	}
 }
 
