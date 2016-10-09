8395ab9cc3c23a792a399bfea53a01327b44e052
Andrew Bezzubtsev <andrew.bezzubtsev@icloud.com>
2016-10-09 17:53:13 +0300

    section list

--- assembler/section/section.hpp
+++ assembler/section/section.hpp
@@ -1,13 +1,56 @@
 #ifndef LLCCEP_ASM_SECTION_HPP
 #define LLCCEP_ASM_SECTION_HPP
 
+#include <string>
+#include <vector>
+
+#include "../declaration/declaration.hpp"
+#include "../statement/statement.hpp"
+
 namespace LLCCEP_ASM {
 	enum section_t {
 		SECTION_T_DECLARATIVE,
 		SECTION_T_IMPERATIVE
 	};
 
-	class section
+	union sectionData {
+		sectionDeclarations *decls;
+		sectionStatements *stats;
+	};
 
+	class section {
+		struct sectionInfo_t {
+			section_t type;
+			sectionData data;
+			::std::string name;
+		} info;
+
+	public:
+		section();
+		~section();
+
+		void setName(::std::string name);
+		void setData(sectionDeclarations *decls);
+		void setData(sectionStatements *stats);
+		void setData(sectionData data);
+
+		::std::string getName() const;
+		section_t getType() const;
+		sectionData getData() const;
+	};
+
+	class sectionList {
+		::std::vector<section *> sections;
+
+	public:
+		sectionList();
+		~sectionList();
+
+		void addSection(section *sect);
+
+		void buildDeclarative(::std::ostream &out) const;
+		void buildImperative(::std::ostream &out) const;
+	};
+}
 
 #endif /* LLCCEP_ASM_SECTION_HPP */
