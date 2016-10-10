7d75bd9681cb508e0cf1c6d1d1be0e08825e087b
Andrew Bezzubtsev <andrew.bezzubtsev@icloud.com>
2016-10-10 21:29:04 +0300

    Remove junk assembler files, from bad version

--- assembler/lib/tools/check/name.hpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include <tools/check/regmatch.hpp>
-#include <tools/check/name.hpp>
-
-bool LLCCEP_ASM::okName(::std::string name)
-{
-	return LLCCEP_ASM::matches(name, "[a-zA-Z_][a-zA-Z_0-9]*");
-}
--- assembler/lib/tools/check/regmatch.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <regex>
-
-#include <tools/check/regmatch.hpp>
-
-bool LLCCEP_ASM::matches(::std::string str, ::std::string regex)
-{
-	return ::std::regex_match(str, ::std::regex(regex, ::std::regex_constants::egrep));
-}
