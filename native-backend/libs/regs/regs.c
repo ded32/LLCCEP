30695a7d7807c1366529f94f4810decf78850c56
Andrew Bezzubtsev <andrew.bezzubtsev@icloud.com>
2016-10-28 22:47:27 +0300

    regs library for backend

---
+++ native-backend/libs/regs/regs.c
@@ -0,0 +1,1 @@
+double regs[32] = {};
---
+++ native-backend/libs/regs/regs.h
@@ -0,0 +1,6 @@
+#ifndef LLCCEP_NATIVE_BACKEND_REGS_H
+#define LLCCEP_NATIVE_BACKEND_REGS_H
+
+extern double regs[32];
+
+#endif /* LLCCEP_NATIVE_BACKEND_REGS_H */
